a7d2ae6a14cc63a27f2ece9ea21ede6c4c8c6292
catry <benjamin.catry@ensta-paris.fr>
2022-05-22 23:04:50 +0200

    modif de fondamentale

--- fondamentale.c
+++ fondamentale.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 
-// retourne la fondamentale du spectre d'un signal en se basant sur la formule fo = argmaxlogprodmodcarréX(if)
+// retourne la fondamentale du spectre d'un signal en cherchant le maximum
 
 
 float fondamentale(float *y, int N, float hf, int taille) 
@@ -13,22 +13,11 @@ float fondamentale(float *y, int N, float hf, int taille)
 
     for (int k =0; k< N/2; k++) {
 
-        T[k]= fabs(y[k]);
+        T[k]= fabs(y[k]); //La transformée de fourier du signal contient des valeurs complexes. Pour déterminer le max, nous passons au module.
 
-      /*  int i=1;
-        float prod = 1;
-        
-        while ((y[k*i+99]!=0) && (k*i<N-100)) {
-            prod = prod*(fabs(y[i*k+99])*fabs(y[i*k+99]));
-            i=i+1;
-        }
-        
-        T[k]=log(prod); 
-
-*/
     }
 
-
+// méthode du maximum : (fonctionnelle)
 
     float max =0;
     int Imax=0;
@@ -41,9 +30,21 @@ float fondamentale(float *y, int N, float hf, int taille)
         }
 
     }
+// méthode du produit spectral ( non fonctionnelle )
+      /*  int i=1;
+        float prod = 1;
+        
+        while ((y[k*i+99]!=0) && (k*i<N-100)) {
+            prod = prod*(fabs(y[i*k+99])*fabs(y[i*k+99]));
+            i=i+1;
+        }
+        
+        T[k]=log(prod); 
+
+*/
 
     float fo = (Imax)*hf/taille;
-    printf("Imax = %d \n", Imax);
+//    printf("Imax = %d \n", Imax);
     free(T);
  //   Ind_max=&Imax;
     return fo;
