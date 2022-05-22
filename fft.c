39033c5dcd3194d1ba5feb1dbc50b85e0a06a061
leomeunier <90618703+leomeunier@users.noreply.github.com>
2022-05-22 21:56:49 +0200

    Update fft.c

--- fft.c
+++ fft.c
@@ -12,13 +12,16 @@ void fft(float data[], unsigned long nn, int isign)
     unsigned long n,mmax,m,j,istep,i;
     double wtemp,wr,wpr,wpi,wi,theta;
     float tempr,tempi;
-    n=nn << 1;
+    n=nn << 1; // décalage d'un bit vers la droite en écriture binaire
     j=1;
+    
+    // boucle permettant de faire un symétrique entre les données afin de mieux les exploiter par la suite lors des sommations
+    
     for (i=1; i<n; i+=2)
     {
         if (j > i)
         {
-            SWAP(data[j],data[i]);
+            SWAP(data[j],data[i]); // Les indices pairs sont les parties réelles et les indices impairs sont les parties imaginaires des données
             SWAP(data[j+1],data[i+1]);
         }
         m=nn;
@@ -33,10 +36,11 @@ void fft(float data[], unsigned long nn, int isign)
     while (n > mmax)
     {
         istep=mmax << 1;
-        theta=isign*(6.28318530717959/mmax);
+        theta=isign*(6.28318530717959/mmax); // theta = pi puis pi/2 ... ( on remultiplie par 1/2 à chaque boucle )
         wtemp=sin(0.5*theta);
-        wpr = -2.0*wtemp*wtemp;
-        wpi=sin(theta);
+        wpr = -2.0*wtemp*wtemp; //Oméga réel ( pour faire la partie réelle dans la fft )
+        wpi=sin(theta); //Oméga imaginaire ( pour faire la partie imaginaire de la fft )
+        
         wr=1.0;
         wi=0.0;
         for (m=1; m<mmax; m+=2)
@@ -44,9 +48,9 @@ void fft(float data[], unsigned long nn, int isign)
             for (i=m; i<=n; i+=istep)
             {
                 j=i+mmax;
-                tempr=wr*data[j]-wi*data[j+1];
-                tempi=wr*data[j+1]+wi*data[j];
-                data[j]=data[i]-tempr;
+                tempr=wr*data[j]-wi*data[j+1]; 
+                tempi=wr*data[j+1]+wi*data[j]; 
+                data[j]=data[i]-tempr; 
                 data[j+1]=data[i+1]-tempi;
                 data[i] += tempr;
                 data[i+1] += tempi;
